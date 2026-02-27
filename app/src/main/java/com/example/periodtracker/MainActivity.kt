dad365fab972b164d8a6c07490a887d652496830
Molly Midkiff <mcmidkiff@wm.edu>
2026-02-27 13:10:56 -0500

    added onboarding, organized navigation

--- app/src/main/java/com/example/periodtracker/MainActivity.kt
+++ app/src/main/java/com/example/periodtracker/MainActivity.kt
@@ -3,15 +3,14 @@ package com.example.periodtracker
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountBox
+import androidx.compose.material.icons.filled.DateRange
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material3.Icon
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.adaptive.navigationsuite.NavigationSuiteScaffold
 import androidx.compose.runtime.Composable
@@ -21,17 +20,20 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewScreenSizes
 import com.example.periodtracker.ui.theme.PeriodTrackerTheme
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.material3.Button
+import androidx.compose.ui.Alignment
+import androidx.compose.foundation.layout.Column
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             PeriodTrackerTheme {
-                PeriodTrackerApp()
+                PeriodTrackerApp(modifier = Modifier.fillMaxSize())
             }
         }
     }
@@ -39,56 +41,73 @@ class MainActivity : ComponentActivity() {
 
 @PreviewScreenSizes
 @Composable
-fun PeriodTrackerApp() {
+fun PeriodTrackerApp(modifier: Modifier = Modifier) {
+
+    var shouldShowOnboarding by rememberSaveable { mutableStateOf(true)}
     var currentDestination by rememberSaveable { mutableStateOf(AppDestinations.HOME) }
+
+    if (shouldShowOnboarding) {
+        OnboardingScreen(
+            onContinueClicked = { shouldShowOnboarding = false}
+        )
+        return
+    }
 
     NavigationSuiteScaffold(
         navigationSuiteItems = {
-            AppDestinations.entries.forEach {
+            AppDestinations.entries.forEach { destination ->
                 item(
                     icon = {
                         Icon(
-                            it.icon,
-                            contentDescription = it.label
+                            destination.icon,
+                            contentDescription = destination.label
                         )
                     },
-                    label = { Text(it.label) },
-                    selected = it == currentDestination,
-                    onClick = { currentDestination = it }
+                    label = { Text(destination.label) },
+                    selected = destination == currentDestination,
+                    onClick = { currentDestination = destination }
                 )
             }
+        } )
+    {
+        when(currentDestination) {
+            AppDestinations.HOME -> HomeScreen()
+            AppDestinations.CALENDAR -> CalendarScreen()
+            AppDestinations.JOURNAL -> JournalScreen()
+            AppDestinations.PROFILE -> ProfileScreen()
         }
+    }
+
+}
+@Composable
+fun OnboardingScreen(onContinueClicked: () -> Unit, modifier: Modifier = Modifier)
+{
+    Column(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-            Greeting(
-                name = "Android",
-                modifier = Modifier.padding(innerPadding)
-            )
+        Text("Tracking your period just got easier.")
+        Button (
+            modifier = Modifier.padding(vertical = 24.dp),
+            onClick = onContinueClicked
+        ) {
+            Text("Continue")
         }
     }
 }
+
+@Composable fun HomeScreen() { Text("Home")}
+@Composable fun CalendarScreen() { Text("Calendar")}
+@Composable fun JournalScreen() { Text("Journal")}
+@Composable fun ProfileScreen() { Text("Profile")}
 
 enum class AppDestinations(
     val label: String,
     val icon: ImageVector,
 ) {
     HOME("Home", Icons.Default.Home),
-    FAVORITES("Favorites", Icons.Default.Favorite),
+    CALENDAR("Calendar", Icons.Default.DateRange),
+    JOURNAL("Journal", Icons.Default.Favorite),
     PROFILE("Profile", Icons.Default.AccountBox),
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    PeriodTrackerTheme {
-        Greeting("Android")
-    }
 }
